31f6ac34a38ec0d43a4e6fc96440eac5ebc189f5
shaswata-26 <106397517+shaswata-26@users.noreply.github.com>
2024-12-02 11:59:27 +0530

    project firebase contact app

---
+++ src/config/firebase.js
@@ -0,0 +1,41 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCw4a_6ouJbvqXnYHuWzDyxtRvFLtwHsS8",
+//   authDomain: "vite-contact.firebaseapp.com",
+//   projectId: "vite-contact",
+//   storageBucket: "vite-contact.appspot.com",
+//   messagingSenderId: "697891519095",
+//   appId: "1:697891519095:web:ba17a86d6965e94c58e472",
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+// export const db = getFirestore(app);
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBHh57BIStvFQ131j-R0asLG3q9iduhu3w",
+  authDomain: "vite-contact-1ad14.firebaseapp.com",
+  projectId: "vite-contact-1ad14",
+  storageBucket: "vite-contact-1ad14.firebasestorage.app",
+  messagingSenderId: "515485634323",
+  appId: "1:515485634323:web:2647b9ec3c72753cedd4e6",
+  measurementId: "G-MB3KDBY727"
+};
+
+// Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
+ export const db = getFirestore(app);
